efef9942da6069fc7315fff5ee7792bbc5a8d459
loganlach <lwlach@gmail.umd.edu>
2023-01-27 13:14:35 -0500

    Initial setup

---
+++ payMeantIOS/payMeant/payMeant/payMeantApp.swift
@@ -0,0 +1,17 @@
+//
+//  payMeantApp.swift
+//  payMeant
+//
+//  Created by Logan Lach on 1/27/23.
+//
+
+import SwiftUI
+
+@main
+struct payMeantApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
